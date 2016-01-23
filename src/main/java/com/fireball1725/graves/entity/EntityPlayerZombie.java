73bd939dc08b8c509671555cad5bb539d6a262a2
FusionLord <FusionLord@live.com>
2016-01-22 19:13:26 -0700

    Changed the super of the twist;

--- src/main/java/com/fireball1725/graves/entity/EntityPlayerZombie.java
+++ src/main/java/com/fireball1725/graves/entity/EntityPlayerZombie.java
@@ -1,96 +1,18 @@
 package com.fireball1725.graves.entity;
 
 import com.mojang.authlib.GameProfile;
-import net.minecraft.block.Block;
-import net.minecraft.entity.EnumCreatureAttribute;
-import net.minecraft.entity.SharedMonsterAttributes;
-import net.minecraft.entity.ai.*;
-import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.monster.EntityPigZombie;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTUtil;
-import net.minecraft.pathfinding.PathNavigateGround;
-import net.minecraft.util.BlockPos;
 import net.minecraft.world.World;
 
-public class EntityPlayerZombie extends EntityMob
+public class EntityPlayerZombie extends EntityPigZombie
 {
 	private GameProfile playerProfile;
 
 	public EntityPlayerZombie(World worldIn)
 	{
 		super(worldIn);
-		((PathNavigateGround) this.getNavigator()).setBreakDoors(true);
-		this.tasks.addTask(0, new EntityAISwimming(this));
-		this.tasks.addTask(2, new EntityAIAttackOnCollide(this, EntityPlayer.class, 1.0D, false));
-		this.tasks.addTask(5, new EntityAIMoveTowardsRestriction(this, 1.0D));
-		this.tasks.addTask(7, new EntityAIWander(this, 1.0D));
-		this.tasks.addTask(8, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
-		this.tasks.addTask(8, new EntityAILookIdle(this));
-		this.applyEntityAI();
-		this.setSize(0.6F, 1.95F);
-	}
-
-	public GameProfile getPlayerProfile()
-	{
-		return playerProfile;
-	}
-
-	public void setPlayerProfile(GameProfile playerProfile)
-	{
-		this.playerProfile = playerProfile;
-	}
-
-	protected void applyEntityAI()
-	{
-		this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true, EntityPigZombie.class));
-		this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityPlayer.class, true));
-	}
-
-	protected void applyEntityAttributes()
-	{
-		super.applyEntityAttributes();
-		this.getEntityAttribute(SharedMonsterAttributes.followRange).setBaseValue(35.0D);
-		this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0.23000000417232513D);
-		this.getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(3.0D);
-	}
-
-	protected void entityInit()
-	{
-		super.entityInit();
-		this.getDataWatcher().addObject(12, Byte.valueOf((byte) 0));
-		this.getDataWatcher().addObject(13, Byte.valueOf((byte) 0));
-		this.getDataWatcher().addObject(14, Byte.valueOf((byte) 0));
-	}
-
-	/**
-	 * Returns the sound this mob makes while it's alive.
-	 */
-	protected String getLivingSound()
-	{
-		return "mob.zombie.say";
-	}
-
-	/**
-	 * Returns the sound this mob makes when it is hurt.
-	 */
-	protected String getHurtSound()
-	{
-		return "mob.zombie.hurt";
-	}
-
-	/**
-	 * Returns the sound this mob makes on death.
-	 */
-	protected String getDeathSound()
-	{
-		return "mob.zombie.death";
-	}
-
-	protected void playStepSound(BlockPos pos, Block blockIn)
-	{
-		this.playSound("mob.zombie.step", 0.15F, 1.0F);
 	}
 
 	@Override
@@ -110,26 +32,4 @@ public class EntityPlayerZombie extends EntityMob
 		NBTUtil.writeGameProfile(profileTag, playerProfile);
 		nbtTagCompound.setTag("playerProfile", profileTag);
 	}
-
-	/**
-	 * Get this Entity's EnumCreatureAttribute
-	 */
-	public EnumCreatureAttribute getCreatureAttribute()
-	{
-		return EnumCreatureAttribute.UNDEAD;
-	}
-
-	public float getEyeHeight()
-	{
-		return 1.74F;
-	}
-
-	/**
-	 * Determines if an entity can be despawned, used on idle far away entities
-	 */
-	protected boolean canDespawn()
-	{
-		return false;
-	}
-
 }
