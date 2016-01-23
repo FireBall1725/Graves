08d8ef1184d4a75efc4766af3a2e53ac609eeef9
FireBall1725 <tylersevdyreed@gmail.com>
2016-01-22 18:52:21 -0800

    Started on journeymap integration

---
+++ src/main/java/com/fireball1725/graves/integrations/JourneyMap.java
@@ -0,0 +1,22 @@
+package com.fireball1725.graves.integrations;
+
+import journeymap.client.api.ClientPlugin;
+import journeymap.client.api.IClientAPI;
+import journeymap.client.api.IClientPlugin;
+import journeymap.client.api.event.ClientEvent;
+
+@ClientPlugin
+public class JourneyMap implements IClientPlugin {
+
+    @Override
+    public void initialize(IClientAPI iClientAPI) {
+
+    }
+
+    @Override
+    public void onEvent(ClientEvent clientEvent) {
+        if (clientEvent.type == ClientEvent.Type.DEATH_WAYPOINT) {
+
+        }
+    }
+}
