d7c68fab7c8ce3ca2cd665739e96b4311203b36a
Taylor Reed <treed@curse.com>
2016-02-03 12:37:26 -0800

    Added configuration file

---
+++ src/main/java/com/fireball1725/graves/configuration/ConfigurationFile.java
@@ -0,0 +1,7 @@
+package com.fireball1725.graves.configuration;
+
+/**
+ * Created by treed on 2/3/16.
+ */
+public class ConfigurationFile {
+}
---
+++ src/main/java/com/fireball1725/graves/helpers/ConfigurationHelper.java
@@ -0,0 +1,7 @@
+package com.fireball1725.graves.helpers;
+
+/**
+ * Created by treed on 2/3/16.
+ */
+public class ConfigurationHelper {
+}
